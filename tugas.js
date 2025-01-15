0bfe25d5676ddff8dcf5a9d49ea0101034c62b32
hennydwi05 <hennydwir9@gmail.com>
2025-01-15 11:31:07 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "henny"; 
+let alamat = "rejosari baru"; 
+let usia = "15";  
+let ttl = "demak 05 maret 2009"; 
+let gender = "perempuan"; 
+let agama = "islam"; 
+let email = "hennydwir9@gmail.com";
+let berat = "38";
+let tinggi = "155";
+let hobi = "scroll sosmed ";
+let cita = "dokter";
+let pekerjaan = "pelajar";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
